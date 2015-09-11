b0806ee04ae68a98721246ca142d710c5c610f03
Archimedes of Syracuse <archimedes@syracuse.(none)>
2015-09-11 10:23:29 +0100

    needless to say, it's that git push time of the day

--- 1st/sim.c
+++ 1st/sim.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include "mips32.h"
 #include <stdint.h>
+#include "elf.h"
 
 #define ERROR_INVALID_ARGS 1
 #define ERROR_IO_ERROR 2
 
+#define MEMSZ sizeof mem;
+
 static uint32_t PC = 0;
 static uint32_t regs[32];
 static size_t instr_cnt = 0;
+static unsigned char mem[640];
 
 int show_status(){
   printf("Executed %zu instruction(s).\n", instr_cnt); 
-  printf("%u\n", PC);
+  printf("PC = 0x%x\n", PC);
+  printf("at = 0x%x\n", regs[1]);
+  printf("v0 = 0x%x\n", regs[2]);
+  printf("v1 = 0x%x\n", regs[3]);
+
+  for (int i = 8; i < 16; i++) {
+    printf("t%d = 0x%x\n",i-8, regs[i]);
+  }
+  printf("sp = 0x%x\n", regs[29]);
+  printf("fp = 0x%x\n", regs[30]);
   
   return 0;
 }
@@ -19,10 +32,13 @@ int show_status(){
 int read_config_stream(FILE *file){
   for (int i = 8; i < 15; i++){
     uint32_t v;
-    if (fscanf(file, "%u", &v) != 0){
+    int count = fscanf(file, "%u", &v);
+    printf("%d\n", count);
+    if (count != 1){
       return 1;
     }
     regs[i] = v;
+    printf("%d\n", v);
   }
 
   if (fclose(file) != 0){
@@ -46,14 +62,15 @@ int read_config(const char *path) {
 }
 
 int main(int argc, char *argv[]){
-  printf("%x\n", PC);
-  regs[1] = regs[1];
-  if(argc == 3){
-    return read_config(argv[1]);
+   if(argc == 3){
+    read_config(argv[1]);
+  } else {
+    printf("Insufficient arguments\n");
+    return ERROR_INVALID_ARGS;
   }
-  printf("Insufficient arguments\n");
-  return ERROR_INVALID_ARGS;
+   show_status();
+   elf_dump(argv[2], &PC, &mem[0], MEMSZ);
 
-  show_status();
+   return 0;
 }
 
