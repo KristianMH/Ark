5772177d1f378f8eb76e2e700f89c8b7af75a55a
Archimedes of Syracuse <archimedes@syracuse.(none)>
2015-09-15 15:00:19 +0100

    c vers 2.0

--- 1st/sim.c
+++ 1st/sim.c
@@ -7,11 +7,11 @@
 #define ERROR_INVALID_ARGS 1
 #define ERROR_IO_ERROR 2
 #define ERROR_UNKNOWN_OPCODE 3
-#define MEMSZ 640*1000
+#define MEMSZ sizeof(mem)/sizeof(mem[0])
 static uint32_t PC = 0;
 static uint32_t regs[32];
 static size_t instr_cnt = 0;
-static unsigned char mem[640*1000];
+static unsigned char mem[640*1024];
 
 int show_status(){
   printf("Executed %zu instruction(s).\n", instr_cnt); 
@@ -58,13 +58,33 @@ int read_config(const char *path) {
   printf("Readfile!\n");
   return 0;
 }
+int intrp_r(uint32_t instr){
+  instr=instr;
+  return 1;
+
+}
 int interp_inst(uint32_t instr){
   //get opcode by GET_UPCODE(instr) and do swicth case
-  printf("%d\n",GET_OPCODE(instr));
   switch(GET_OPCODE(instr)){
   case FUNCT_SYSCALL :
     return 1;
-    
+  case OPCODE_R:
+    return intrp_r(instr);
+    //many other cases to be implemented
+  case OPCODE_J :
+    return 0;
+  case OPCODE_JAL :
+    return 0;
+  case OPCODE_BEQ:
+    return 0;
+  case OPCODE_BNE:
+    return 0;
+  case OPCODE_ADDIU:
+    return 0;
+  case OPCODE_SW:
+    return 0;
+  case OPCODE_LW:
+    return 0;
   default : 
     return ERROR_UNKNOWN_OPCODE;
   }
@@ -73,35 +93,35 @@ int interp_inst(uint32_t instr){
 }
 int interp(){
   for (;;){
+    instr_cnt ++;
     int count = interp_inst(GET_BIGWORD(mem,PC)); // gets instr from PC in reg array (0)
     if (count == 1){ //sees the syscall function;
       printf("hello this is the syscall function\n");
       break;
     }
     if (count == ERROR_UNKNOWN_OPCODE){ //error occured eg. unsupported function
-      // do we return or break??return 1;
       return ERROR_UNKNOWN_OPCODE;
     }
-        instr_cnt ++;
+        
   }
   return 0;
 }
 int main(int argc, char *argv[]){
-   if(argc == 3){
-     if(read_config(argv[1])==0){ // checks return value of read cfg FILE.
-       int elfreturn = elf_dump(argv[2], &PC, &mem[0], MEMSZ);
-       if (elfreturn == 0){ 
-	 // checks elf_dump, reads the elf FILE?
-	 regs[29] = (uint32_t)mem[MEMSZ-4];
-	 interp();
-	 return show_status();
-	 // implement initalize of stack-pointer.
-	 // and call interp to read
-       }
-     }
-   } else {
+   if(argc != 3){
      printf("Insufficient arguments\n");
      return ERROR_INVALID_ARGS;
+   }
+   // checks return value of read cfg FILE.
+   if(read_config(argv[1])==0){ 
+     //return value of elf_dump. 
+     int elfreturn = elf_dump(argv[2], &PC, &mem[0], MEMSZ);
+     if (elfreturn == 0){ 
+       regs[29] = MIPS_RESERVE+MEMSZ-4;
+       interp();
+       return show_status();
+       // implement initalize of stack-pointer.
+       // and call interp to read
+     }
    }
    
    //note: BRUG GET_BIGWORD OG SET_BIGWORD, når der hentes/indsættes i mem   
