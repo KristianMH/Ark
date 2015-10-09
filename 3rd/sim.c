a7a008ddc6a9e93683a8ab32cfb074a5ddd17055
KristianMH <krillemh@gmail.com>
2015-10-09 11:56:38 +0200

    G3 dONE?

---
+++ 3rd/sim.c
@@ -0,0 +1,542 @@
+#include <stdio.h>
+#include "mips32.h"
+#include <stdint.h>
+#include "elf.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#define ERROR_INVALID_ARGS 1
+#define ERROR_IO_ERROR 2
+#define ERROR_UNKNOWN_OPCODE 3
+#define ERROR_UNKNOWN_FUNCT 4
+#define MEMSZ 640*1024
+#define SAW_SYSCALL 5
+static uint32_t PC;
+static uint32_t regs[32];
+static size_t instr_cnt = 0;
+static unsigned char mem[MEMSZ];
+static size_t cycles_cnt = 0;
+struct preg_if_id {
+  uint32_t inst;
+  uint32_t next_pc;
+};
+static struct preg_if_id if_id;
+struct preg_id_ex {
+  bool branch;
+  bool alu_src;
+  bool mem_read;
+  bool mem_write;
+  bool reg_write;
+  bool mem_to_reg;
+  bool jump;
+  uint32_t jump_target;
+  uint32_t rt; // = GET_RT(if_id.inst);
+  uint32_t rs;
+  uint32_t rs_value;// = regs[GET_RS(if_id.inst)];
+  uint32_t rt_value;// = regs[rt];
+  uint32_t sign_ext_imm; //= SIGN_EXTEND(GET_IMM(if_id.inst));
+  uint32_t funct;
+  uint32_t reg_dst;
+  uint32_t shamt;
+  uint32_t next_pc;
+};
+static struct preg_id_ex id_ex;
+struct preg_ex_mem {
+  bool mem_read;
+  bool mem_write;
+  bool reg_write;
+  bool mem_to_reg;
+  bool branch;
+  uint32_t rt;// = GET_RT(if_id.inst);
+  uint32_t rt_value;// = regs[rt];
+  uint32_t alu_res; //result of alu()
+  uint32_t reg_dst;
+  uint32_t branch_target;
+    
+};
+static struct preg_ex_mem ex_mem;
+struct preg_mem_wb {
+  bool reg_write;
+  bool mem_to_reg;
+  bool mem_write;
+  bool mem_read;
+  uint32_t rt;
+  uint32_t read_data;
+  uint32_t alu_res; // result of alu()
+  uint32_t rt_value;
+  uint32_t reg_dst;
+};
+static struct preg_mem_wb mem_wb;
+int show_status(){
+  printf("Executed %zu instruction(s).\n", instr_cnt); 
+  printf("Executed %zu cycle(s).\n", cycles_cnt);
+  printf("PC = 0x%x\n", PC);
+  printf("at = 0x%x\n", regs[1]);
+  printf("v0 = 0x%x\n", regs[2]);
+  printf("v1 = 0x%x\n", regs[3]);
+
+  for (int i = 8; i < 16; i++) {
+    printf("t%d = 0x%x\n",i-8, regs[i]);
+  }
+  printf("sp = 0x%x\n", regs[29]);
+  printf("fp = 0x%x\n", regs[30]);
+  printf("ra = 0x%x\n", regs[31]);
+  
+  return 0;
+}
+// reads the variables from cfg file
+int read_config_stream(FILE *file){
+  for (int i = 8; i < 16; i++){
+    uint32_t v;
+    int count = fscanf(file, "%u", &v);
+    // checks if fscanf failed to read
+    if (count != 1){
+      return 1;
+    }
+    regs[i] = v;
+  }
+
+  return 0;
+}
+// for bug-fixing. some bools / values from pipe-regs may be missing.
+void show_regs_status(){
+  printf("cycle nr. = %zu\n",instr_cnt);
+  printf("VALUES OF IF/ID\n");
+  printf("value of IF/ID.inst = %x\n",if_id.inst);
+  printf("value of IF/ID.next_pc = %x\n", if_id.next_pc);
+  printf("VALUES OF ID/EX\n");
+  printf("value of ID/EX.branch = %d\n", id_ex.branch);
+  printf("value of ID/EX.alu_src = %d\n", id_ex.alu_src);
+  printf("value of ID/EX.mem_read = %d\n",id_ex.mem_read);
+  printf("value of ID/EX.mem_write = %d\n", id_ex.mem_write);
+  printf("value of ID/EX.reg_write = %d\n", id_ex.reg_write);
+  printf("value of ID/EX.rt = %x\n",id_ex.rt);
+  printf("value of ID/EX.rs = %x\n",id_ex.rs_value);
+  printf("value of ID/EX.rt_value = %x\n",id_ex.rt_value);
+  printf("value of ID/EX.sign_ext_imm = %x\n", id_ex.sign_ext_imm);
+  printf("value of ID/EX.funct = %x\n",id_ex.funct);
+  printf("value of ID/EX.reg_dst = %x\n",id_ex.reg_dst);
+  printf("value of ID/EX.shamt = %x\n", id_ex.shamt);
+  printf("value of ID/EX.next_pc = %x\n", id_ex.next_pc);
+  printf("value of ID/EX.jump_target = %x\n",id_ex.jump_target);
+  printf("VALUES OF EX/MEM\n");  
+  printf("value of EX/MEM.mem_read = %d\n", ex_mem.mem_read);
+  printf("value of EX/MEM.mem_write = %d\n", ex_mem.mem_write);
+  printf("value of EX/MEM.reg_write = %d\n", ex_mem.reg_write);
+  printf("value of EX/MEM.branch = %d\n", ex_mem.branch);
+  printf("value of EX/MEM.rt = %x\n",ex_mem.rt);
+  printf("value of EX/MEM.rt_value = %x\n",ex_mem.rt_value);
+  printf("value of EX/MEM.alu_res = %x\n",ex_mem.alu_res);
+  printf("value of EX/MEM.reg_dst = %x\n",ex_mem.reg_dst);
+  printf("value of EX/MEM.branch_target = %x\n", ex_mem.branch_target);
+  printf("VALUES OF MEM/WB \n");
+  printf("value of MEM/WB.reg_write =%d\n",mem_wb.reg_write);
+  printf("value of MEM/WB.mem_to_reg =%d\n",mem_wb.mem_to_reg);
+  printf("value of MEM/WB.rt =%x\n",mem_wb.rt);
+  printf("value of MEM/WB.read_data = %x\n", mem_wb.read_data);
+  printf("value of MEM/WB.alu_res =%d\n",mem_wb.alu_res);
+  printf("value of MEM/WB.reg_dst =%x\n",mem_wb.reg_dst);
+  printf("value of MEM/WB.mem_write =%d\n",mem_wb.mem_write);
+  printf("value of MEM/WB.mem_read =%d\n",mem_wb.mem_read);
+  printf("END OF CYCLE\n");
+}
+int read_config(const char *path) {
+  
+  FILE *fdesc = fopen(path, "r");
+  if(fdesc == NULL){
+    printf("Could not open file\n");
+    return ERROR_IO_ERROR;
+  }
+
+  if (read_config_stream(fdesc) == 1){
+    printf("unsuccesfully initialized registers, check cfg file");
+    return ERROR_IO_ERROR;
+  }
+  if (fclose(fdesc) != 0) {
+    return ERROR_IO_ERROR;
+  }
+  return 0;
+}
+
+void interp_wb(){
+  if (!(mem_wb.reg_write) || mem_wb.reg_dst == 0){
+    return;
+  }
+  if (mem_wb.mem_to_reg) {
+    regs[mem_wb.reg_dst] = mem_wb.read_data;
+  } else {
+    regs[mem_wb.reg_dst] = mem_wb.alu_res;
+  }
+  
+}
+void interp_mem(){
+  mem_wb.mem_write = ex_mem.mem_write;
+  mem_wb.mem_read = ex_mem.mem_read;
+  mem_wb.reg_write = ex_mem.reg_write;
+  mem_wb.rt = ex_mem.rt;
+  mem_wb.rt_value = ex_mem.rt_value;
+  mem_wb.mem_to_reg = ex_mem.mem_to_reg;
+  mem_wb.reg_dst = ex_mem.reg_dst;
+  mem_wb.alu_res = ex_mem.alu_res;
+  // write or read from memory
+  if (mem_wb.mem_read){
+    mem_wb.read_data = GET_BIGWORD(mem,mem_wb.alu_res);
+  }
+  if (mem_wb.mem_write) {
+    SET_BIGWORD(mem,mem_wb.alu_res,mem_wb.rt_value);
+  }
+  
+}
+void interp_if(){
+  uint32_t addr = GET_BIGWORD(mem,PC);
+  if_id.inst = addr;
+  if (id_ex.mem_read && (id_ex.rs == GET_RS(addr) || id_ex.rt == GET_RT(addr))) {
+    //insert nop
+    if_id.inst = 0;
+    return;
+  }
+  PC+=4;
+  if_id.next_pc = PC;
+  instr_cnt ++;
+}
+int interp_control(){
+  switch (GET_OPCODE(if_id.inst)){
+  case OPCODE_LW:
+    id_ex.mem_read = 1;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 1;
+    id_ex.mem_to_reg = 1;
+    id_ex.funct = FUNCT_ADD;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.branch = 0;
+    return 0;
+  case OPCODE_SW:
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 1;
+    id_ex.reg_write = 0;
+    id_ex.mem_to_reg = 0;
+    id_ex.funct = FUNCT_ADD;
+    id_ex.alu_src = 1;
+    id_ex.branch = 0;
+    id_ex.jump = 0;
+    return 0;
+  case OPCODE_R:
+    id_ex.alu_src = 0;
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 1;
+    id_ex.mem_to_reg = 0;
+    id_ex.branch = 0;
+    id_ex.jump = 0;
+    id_ex.reg_dst = GET_RD(if_id.inst);
+    id_ex.funct = GET_FUNCT(if_id.inst);
+    id_ex.shamt = GET_SHAMT(if_id.inst);
+    if (id_ex.funct == FUNCT_JR) {
+      id_ex.jump = 1;
+      id_ex.jump_target = regs[GET_RS(if_id.inst)];
+    }
+    return 0;
+  case OPCODE_BEQ:
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 0;
+    id_ex.branch = 1;
+    id_ex.jump = 0;
+    id_ex.funct = FUNCT_SUB;
+    return 0;
+  case OPCODE_BNE:
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 0;
+    id_ex.branch = 1;
+    id_ex.jump = 0;
+    id_ex.funct = FUNCT_SUB;
+    return 0;
+  case OPCODE_J:
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 0;
+    id_ex.branch = 0;
+    id_ex.jump = 1;
+    id_ex.jump_target = (if_id.next_pc & MS_4B) | (GET_ADDRESS(if_id.inst) << 2);
+    return 0;
+  case OPCODE_JAL:
+    id_ex.mem_read = 0;
+    id_ex.mem_write = 0;
+    id_ex.reg_write = 1;
+    id_ex.branch = 0;
+    id_ex.jump = 1;
+    id_ex.funct = FUNCT_ADD;
+    id_ex.reg_dst = 31;
+    id_ex.alu_src=0;
+    id_ex.rs_value = if_id.next_pc;
+    id_ex.rt_value = 0;
+    id_ex.jump_target = (if_id.next_pc & MS_4B) | (GET_ADDRESS(if_id.inst) << 2);
+    return 0; 
+  case OPCODE_ADDI:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_ADD;
+    return 0;
+  case OPCODE_ADDIU:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_ADDU;
+    return 0;
+  case OPCODE_ANDI:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_AND;
+    return 0;
+  case OPCODE_LUI:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = 16; //special case for lui
+    return 0;
+  case OPCODE_ORI:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_OR;
+    return 0;
+  case OPCODE_SLTI:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_SLT;
+    return 0;
+  case OPCODE_SLTIU:
+    id_ex.reg_write = 1;
+    id_ex.reg_dst = GET_RT(if_id.inst);
+    id_ex.alu_src = 1;
+    id_ex.funct = FUNCT_SLTU;
+    return 0;
+  default:
+    return ERROR_UNKNOWN_OPCODE;
+  }
+}
+int alu(){
+  uint32_t second;
+  // chose immediate or rt value
+  if (id_ex.alu_src) {
+    second = id_ex.sign_ext_imm;
+  } else {
+    second = id_ex.rt_value;
+  }
+  switch(id_ex.funct){
+  case FUNCT_ADD:
+    ex_mem.alu_res = second + id_ex.rs_value;
+    return 0;
+  case FUNCT_ADDU:
+    ex_mem.alu_res = second + id_ex.rs_value;
+    return 0;
+  case FUNCT_SYSCALL:
+    return SAW_SYSCALL;
+  case FUNCT_AND:
+    ex_mem.alu_res = second & id_ex.rs_value;
+    return 0;
+  case FUNCT_OR:
+    ex_mem.alu_res = second | id_ex.rs_value;
+    return 0;
+  case FUNCT_NOR:
+    ex_mem.alu_res = ~(second | id_ex.rs_value);
+    return 0;
+  case FUNCT_SLL:
+    ex_mem.alu_res = second << id_ex.shamt;
+    return 0;
+  case FUNCT_SLT:
+    ex_mem.alu_res = (second > id_ex.rs_value) ? 1 : 0;
+    return 0;
+  case FUNCT_SLTU:
+    ex_mem.alu_res = (second > id_ex.rs_value) ? 1 : 0;
+    return 0;
+  case FUNCT_SRL:
+    ex_mem.alu_res = second >> id_ex.shamt;
+    return 0;
+  case FUNCT_SUB:
+    ex_mem.alu_res = id_ex.rs_value - second;
+    return 0;
+  case FUNCT_SUBU:
+    ex_mem.alu_res = id_ex.rs_value - second;
+    return 0;
+  case FUNCT_JR:
+    return 0;
+  case 16:
+    // created for implementation of lui
+    ex_mem.alu_res = second << 16;
+    return 0;
+  default:
+    return ERROR_UNKNOWN_FUNCT;
+  }
+  
+}
+int interp_ex(){
+  ex_mem.mem_read = id_ex.mem_read;
+  ex_mem.mem_write = id_ex.mem_write;
+  ex_mem.reg_write = id_ex.reg_write;
+  ex_mem.mem_to_reg = id_ex.mem_to_reg;
+  ex_mem.reg_dst = id_ex.reg_dst;
+  ex_mem.branch = id_ex.branch;
+  ex_mem.rt_value = id_ex.rt_value;
+  if (ex_mem.branch) {
+    ex_mem.branch_target = id_ex.next_pc + (id_ex.sign_ext_imm << 2);
+  }
+  int alureturn = alu();
+  if (alureturn != 0) {
+    return alureturn;
+  }
+  return 0;
+}
+int interp_id(){
+  id_ex.rt = GET_RT(if_id.inst);
+  id_ex.rs_value = regs[GET_RS(if_id.inst)];
+  id_ex.rt_value = regs[id_ex.rt];
+  id_ex.sign_ext_imm = SIGN_EXTEND(GET_IMM(if_id.inst));
+  id_ex.next_pc = if_id.next_pc;
+  id_ex.rs = GET_RS(if_id.inst);
+  int returnvalue = interp_control();
+  
+  if (returnvalue != 0){
+    return returnvalue;
+  }
+  return 0;
+}
+int detectexHazard(){
+  // returns nonzero value of ex-hazards where detected
+  int returnvalue = 0;
+  if (ex_mem.reg_write && ex_mem.reg_dst == 0) {
+    return returnvalue;
+  }
+  if (ex_mem.reg_write && ex_mem.reg_dst == id_ex.rs) {
+    id_ex.rs_value = ex_mem.alu_res;
+    returnvalue ++;
+  }
+  if (ex_mem.reg_write && ex_mem.reg_dst == id_ex.rt) {
+    id_ex.rt_value = ex_mem.alu_res;
+    returnvalue++;
+  }
+  return returnvalue;
+}
+int detectmemHazard(){
+  // returns nonzero value of mem-hazards where detected
+  int returnvalue = 0;
+  if (!mem_wb.reg_write) {
+    return returnvalue;
+  }
+  if (mem_wb.reg_dst == 0) {
+    return returnvalue;
+  }
+  // rs
+  if (mem_wb.reg_dst == id_ex.rs &&
+      !(ex_mem.reg_write && ex_mem.reg_dst != 0 && ex_mem.reg_dst != id_ex.rs)) {
+    returnvalue++;
+    if (mem_wb.mem_to_reg) {
+      id_ex.rs_value = mem_wb.read_data;
+    } else {
+      id_ex.rs_value = mem_wb.alu_res;
+    }
+  }
+  
+  if (mem_wb.reg_dst == id_ex.rt &&
+      !(ex_mem.reg_write && ex_mem.reg_dst != 0 && ex_mem.reg_dst != id_ex.rt)) {
+    returnvalue++;
+    if (mem_wb.mem_to_reg) {
+      id_ex.rt_value = mem_wb.read_data;
+    } else {
+      id_ex.rt_value = mem_wb.alu_res;
+    }
+  }
+  if (GET_FUNCT(if_id.inst) == FUNCT_JR && mem_wb.reg_write && mem_wb.reg_dst == 31) {
+    id_ex.jump_target = mem_wb.alu_res;
+    returnvalue++;
+  }
+  return returnvalue;
+}
+void forward(){
+  if (detectexHazard() != 0) {
+    return;
+  }
+  detectmemHazard();
+  return;
+}
+int cycle(){
+  interp_wb();
+  interp_mem();
+  int returnvalue = interp_ex();
+  if (returnvalue != 0) {
+    return returnvalue;
+  }
+  returnvalue = interp_id();
+  // error-check
+  if (returnvalue != 0){
+    return returnvalue;
+  }
+  interp_if();
+  // beq
+  if (ex_mem.branch && ex_mem.alu_res == 0) {
+    PC = ex_mem.branch_target;
+    if_id.inst = 0;
+    instr_cnt --;
+  }
+  // bne
+  if (ex_mem.branch && ex_mem.alu_res != 0){
+    PC = ex_mem.branch_target;
+    if_id.inst = 0;
+    instr_cnt --;
+  }
+  // jump
+  if (id_ex.jump ) {
+    PC = id_ex.jump_target;
+  }
+  // for bug-fixing ;
+  show_regs_status();
+  forward();
+  return 0;
+}
+int interp(){
+  //for-ever loop
+  int cyclesreturn;
+  for (;;){
+    cycles_cnt ++;
+    cyclesreturn = cycle();
+    if (cyclesreturn != 0){ 
+      break;
+    }
+  }
+  if (cyclesreturn != 0) {
+    return cyclesreturn;
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]){
+   if(argc != 3){
+     printf("Insufficient arguments\n");
+     return ERROR_INVALID_ARGS;
+   }
+   // checks return value of read cfg FILE.
+   if(read_config(argv[1]) != 0) {
+     return ERROR_IO_ERROR;
+   } 
+     //return value of elf_dump. 
+   int elfreturn = elf_dump(argv[2], &PC, &mem[0], MEMSZ);
+   if (elfreturn != 0){
+     printf("elf_dump failed");
+     return 1;
+   } 
+   // sets the stack-pointer
+   regs[29] = MIPS_RESERVE+MEMSZ;
+   // return value of interp
+   int interpreturn = interp();
+   if (interpreturn == 0 || interpreturn == 5){
+     return show_status();
+   }
+   return interpreturn;
+}
+
+
+
